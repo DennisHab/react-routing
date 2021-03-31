f5dcf3c96703f2f1a6ec520c710e5af2fbcd2ab5
DennisHab <dennis_habets@hotmail.com>
2021-03-31 23:52:55 +0200

    Ready for review

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { useState } from 'react';
 import './App.css';
+import TopMenu from "./components/TopMenu";
+import BlogOverview from "./pages/BlogOverview";
+import Blogposts from "./pages/Blogposts";
+import Home from "./pages/Home";
+import PrivateRoute from "./components/PrivateRoute";
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 
 function App() {
-  // We houden in de state bij of iemand is "ingelogd" (simpele versie)
-  const [isAuthenticated, toggleIsAuthenticated ] = useState(false);
-
+    const [isAuthenticated, toggleIsAuthenticated ] = useState(false);
   return (
-    <div>
-      Maak hier jouw prachtige blog-applicatie!
-    </div>
+      <Router>
+          <TopMenu logState={isAuthenticated} toggleLogState = {toggleIsAuthenticated} />
+          <Switch>
+              <Route exact path="/">
+                  <Home />
+              </Route>
+              <Route exact path="/blog-overview">
+                  <BlogOverview logState={isAuthenticated}/>
+              </Route>
+              <PrivateRoute
+                  logState={isAuthenticated}
+                  path="/blog-posts/:id"
+                  component={<Blogposts logState={isAuthenticated} />}
+              />
+          </Switch>
+      </Router>
   );
 }
-
 export default App;
+
+
